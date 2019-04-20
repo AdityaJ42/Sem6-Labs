963bbde839d1a5b122fd883bc7808cfc42bdfcef
AdityaJ42 <adit98@gmail.com>
2019-04-20 16:04:45 +0530

    Simple Left Recursion

--- SPCC/left_rec.py
+++ SPCC/left_rec.py
@@ -1,28 +1,28 @@
-# Eliminate Left Recursion from a grammar
-n = int(input('Enter number of rules: '))
+# Left Recursion
+
+n = int(input('Enter the number of rules: '))
 rules = {}
 for i in range(n):
-    a, b = map(str.strip, input('Enter rule ' + str(i + 1) + ': ').split('->'))
-    rules[a] = list(map(list, map(str.strip, b.split('|'))))
+    prod = input('Enter rule {}: '.format(i + 1)).split('->')
+    rules[prod[0]] = prod[1].split('|')
+temp = rules.copy()
 
-x = rules.copy()
-for i in x:
+for i in temp:
     rec = []
     rules[i + "'"] = []
-    for j in x[i]:
+
+    for j in temp[i]:
         if j[0] == i:
-            rules[i + "'"].append(j[1:] + [i + "'"])
+            rules[i + "'"].append(j[1:] + (i + "'"))
             rec.append(j)
     if rec:
-        rules[i + "'"].append(['$'])
+        rules[i + "'"].append('@')
         for j in rec:
             rules[i].remove(j)
-        for k in x[i]:
-            k.append(i + "'")
+        for k in range(len(rules[i])):
+            rules[i][k] += i + "'"
     else:
         del rules[i + "'"]
 
 for i in rules:
-    for j in range(len(rules[i])):
-        rules[i][j] = ''.join(rules[i][j])
-    print(i + ' -> ' + ' | '.join(map(str, rules[i])))
+    print('{} -> {}'.format(i, '|'.join(rules[i])))
