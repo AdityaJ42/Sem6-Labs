fe9d5c23dbcda03dbb9f37a214f1cd0617bfcb5e
AdityaJ42 <adit98@gmail.com>
2019-04-23 19:40:46 +0530

    Left factoring

--- SPCC/left_fact.py
+++ SPCC/left_fact.py
@@ -1,29 +1,45 @@
-# Left Factoring (works for single common variable)
+# Left Factoring
+from os.path import commonprefix
+
+
 def factor(lhs, rhs):
-	global count
-	visited, variables, beta = [0 for i in rhs], {}, []
+	variables = {}
 	for i in range(len(rhs)):
-		if visited[i] == 0:
-			flag, visited[i], rule = 0, 1, rhs[i]
-			for j in range(i + 1, len(rhs)):
-				if visited[j] == 0 and rule[0] == rhs[j][0]:
-					visited[j], flag = 1, 1
-					variables[rule[0]] = variables.get(rule[0], []) + [rhs[j][1:]]
-			if flag:
-				variables[rule[0]] = variables.get(rule[0], []) + [rule[1:]]
-			else:
-				beta.append(rule)
+		for j in range(len(rhs)):
+			if i != j:
+				prefix = commonprefix([rhs[i], rhs[j]])
+				if prefix:
+					if prefix in variables.keys():
+						variables[prefix].add(i)
+						variables[prefix].add(j)
+					else:
+						variables[prefix] = set([i, j])
 
-	for key in variables:
-		count += 1
-		lhs_new = lhs + "'" * (count - lhs.count("'"))
-		print('{} -> {}'.format(lhs, key + lhs_new))
-		factor(lhs_new, variables[key])
-	for rem in beta:
-		print('{} -> {}'.format(lhs, rem))
+	final_order = [k for k in sorted(variables, key=lambda k: len(variables[k]), reverse=True)]
+	visited = [0 for i in rhs]
+	global count
 
-prod = 'A->aB|aC|aD|Z'
-prod = prod.split('->')
-l, r = prod[0], prod[1].split('|')
+	for key in final_order:
+		temp = []
+		for i in variables[key]:
+			if visited[i]:
+				break
+			visited[i] = 1
+			temp.append(rhs[i][len(key):])
+		if temp:
+			count += 1
+			lhs_new = lhs + "'" * (count - lhs.count("'"))
+			print('{} -> {}'.format(lhs, key + lhs_new))
+			factor(lhs_new, temp)
+
+	for i in range(len(rhs)):
+		if not visited[i]:
+			print('{} -> {}'.format(lhs, rhs[i]))
+
+s = 'S -> bSSaaS | bSSaSb | bSb | a'
+v = s.split('->')
+lhs, rhs = v[0].strip(), v[1].strip()
+rhs = [x.strip() for x in rhs.split('|')]
+
 count = 0
-factor(l, r)
+factor(lhs, rhs)
